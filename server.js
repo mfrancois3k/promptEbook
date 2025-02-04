a8860f12b703ca3b04eba8bda779fd220530dea9
Michael <mfrancois3k@gmail.com>
2025-02-04 16:07:39 -0500

    update 9 endpoint

--- server.js
+++ server.js
@@ -20,28 +20,28 @@ app.get('/download-file', (req, res) => {
         console.log('Server started on PORT ${PORT}') 
     });
  
-    export const config = {
-    api: {
-      responseLimit: '50mb',
-      bodyParser: false,
-    },
-  };
+//     export const config = {
+//     api: {
+//       responseLimit: '50mb',
+//       bodyParser: false,
+//     },
+//   };
   
-  export default async function handler(req, res) {
-      const GOOGLE_DRIVE_URL = 'https://drive.google.com/file/d/1AjXetu6N3HZVbTtnbONFZTE89gy3uOLK/view?usp=sharing';
+//   export default async function handler(req, res) {
+//       const GOOGLE_DRIVE_URL = 'https://drive.google.com/file/d/1AjXetu6N3HZVbTtnbONFZTE89gy3uOLK/view?usp=sharing';
   
-      try {
-          const response = await fetch(GOOGLE_DRIVE_URL);
-          if (!response.ok) throw new Error('Failed to fetch PDF');
+//       try {
+//           const response = await fetch(GOOGLE_DRIVE_URL);
+//           if (!response.ok) throw new Error('Failed to fetch PDF');
           
-          const pdfData = await response.blob();
+//           const pdfData = await response.blob();
           
-          res.setHeader('Content-Type', 'application/pdf');
-          res.setHeader('Content-Disposition', 'attachment; filename=AI-Genius-AI-Prompts-For-Creators.pdf');
+//           res.setHeader('Content-Type', 'application/pdf');
+//           res.setHeader('Content-Disposition', 'attachment; filename=AI-Genius-AI-Prompts-For-Creators.pdf');
           
-          const arrayBuffer = await pdfData.arrayBuffer();
-          res.send(Buffer.from(arrayBuffer));
-      } catch (error) {
-          res.status(500).json({ error: 'Error downloading file' });
-      }
-  }
+//           const arrayBuffer = await pdfData.arrayBuffer();
+//           res.send(Buffer.from(arrayBuffer));
+//       } catch (error) {
+//           res.status(500).json({ error: 'Error downloading file' });
+//       }
+//   }
